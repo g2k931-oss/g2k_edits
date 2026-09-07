ba03ef03c5429965a89c7ed63f8b0bba99554488
g2k931-oss <g2k931@gmail.com>
2026-09-07 20:28:42 +0530

    Delete abhishek.js

--- abhishek.js
+++
@@ -1,6 +0,0 @@
-let paybtn = document.querySelector("button");
-let amount = document.getElementById("amount");
-let amountentered = amount.value;
-paybtn.addEventListener("click", ()=> {
-    window.location.href = `upi://pay?pa=abhishekidkshina@fam&pn=Abhishek%20Maitra&am=${amountentered}&cu=INR`;
-});
